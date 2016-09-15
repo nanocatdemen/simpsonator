f5b4115dbec00b5f5b4bda13e59ba2a75a29f335
jaimesanz <jimbosanz23@gmail.com>
2016-09-15 00:15:42 -0300

    add function to find frames where a specifyc word was used

--- src/subtitles/test_utils.py
+++ src/subtitles/test_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-from subtitles.utils import parse_subs
+from subtitles.utils import parse_subs, find_frames
 
 
 class ParseSubsTest(TestCase):
@@ -33,3 +33,24 @@ class ParseSubsTest(TestCase):
 		self.assertEqual(sub.end.hours, 0)
 		self.assertEqual(sub.end.minutes, 36)
 		self.assertEqual(sub.end.seconds, 3)
+
+
+class PlayVideoTest(TestCase):
+
+	def setUp(self):
+		""" Opens the substitles file and the video. """
+		self.video_filename = "tests/test_0.mkv"
+		self.subs_filename = "tests/test_0.srt"
+		self.subs_obj = parse_subs(self.subs_filename)
+
+	def test_find_obama_frames(self):
+		obama_mentioned_in_frames = []
+		for sub in self.subs_obj:
+			if "Obama" in sub.text:
+				start_in_second = sub.start.hours * 3600 + sub.start.minutes * 60 + sub.start.seconds
+				end_in_seconds = sub.end.hours * 3600 + sub.end.minutes * 60 + sub.end.seconds
+				start_frame = 24 * start_in_second
+				end_frame = 24 * end_in_seconds
+				obama_mentioned_in_frames.append((start_frame, end_frame))
+		# [(3456, 3504), (4200, 4248), (4464, 4536)]
+		find_frames(self.video_filename, obama_mentioned_in_frames)
--- src/subtitles/utils.py
+++ src/subtitles/utils.py
@@ -2,12 +2,36 @@
 import os
 
 import pysrt
+import numpy as np
+import cv2
 
 from django.conf import settings
 
 
 def parse_subs(filename):
 	""" Opens a SRT file. """
+	# TODO remove weird characters like '\n' and HTML tags
 	full_path = os.path.join(settings.MEDIA_ROOT, filename)
 	subs = pysrt.open(full_path)
 	return subs
+
+
+def find_frames(video_filename, frame_ranges):
+	""" Given a video an a list of frame ranges, finds the frames and shows them. """
+	full_path = os.path.join(settings.MEDIA_ROOT, video_filename)
+	cap = cv2.VideoCapture(full_path)
+
+	fps = cap.get(cv2.CAP_PROP_FPS)
+
+	frame_number = 0
+	while cap.isOpened():
+		ret, frame = cap.read()
+
+		if any(frame_number in range(fr[0], fr[1]) for fr in frame_ranges):
+			cv2.imshow('frame', frame)
+			if cv2.waitKey(10) & 0xFF == ord('q'):
+				break
+		frame_number += 1
+
+	cap.release()
+	cv2.destroyAllWindows()
